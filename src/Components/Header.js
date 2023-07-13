3c3f1f572a6ab6d5654e8c4dda9a7f4e40deeb8f
Zeeshan1261 <zk12697@gmail.com>
2023-07-13 18:00:37 +0530

    updating

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,12 @@
-import React,{useState} from "react";
-import { Navbar, Container, Nav, } from "react-bootstrap";
-import Badge from '@mui/material/Badge';
-import {NavLink}  from "react-router-dom";
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from "react";
+import "./style.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import Badge from "@mui/material/Badge";
+import { NavLink } from "react-router-dom";
+import Menu from "@mui/material/Menu";
+// import MenuItem from "@mui/material/MenuItem";
 function Header() {
-
-  const [anchorEl, setAnchorEl] = useState()
+  const [anchorEl, setAnchorEl] = useState();
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -15,21 +15,59 @@ function Header() {
     setAnchorEl(null);
   };
 
-
-
   return (
     <div>
-      <Navbar bg="dark" data-bs-theme="dark" style={{height:"60px"}}>
+      <Navbar bg="dark" data-bs-theme="dark" style={{ height: "60px" }}>
         <Container>
-          <NavLink className={'text-decoration-none text-light mx-3'} to='/' href="#home ">Add To Cart</NavLink>
+          <NavLink
+            className={"text-decoration-none text-light mx-3"}
+            to="/"
+            href="#home "
+          >
+            Add To Cart
+          </NavLink>
           <Nav className="me-auto">
-            <NavLink to='/' className={'text-decoration-none text-light'} href="#home">Home</NavLink>
+            <NavLink
+              to="/"
+              className={"text-decoration-none text-light"}
+              href="#home"
+            >
+              Home
+            </NavLink>
           </Nav>
-          <Badge badgeContent={1} color="primary">
-          <i class="fa-solid fa-cart-shopping text-light " style={{fontSize:25,cursor:'pointer'}}></i>
-    </Badge>
+          <Badge
+            badgeContent={1}
+            color="primary"
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            <i
+              class="fa-solid fa-cart-shopping text-light "
+              style={{ fontSize: 25, cursor: "pointer" }}
+            ></i>
+          </Badge>
         </Container>
-
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <div className="card_details">
+            <i
+              className="fas fa-close smallclose"
+              style={{ position: "absolute", top: 2, right: 20, fontSize: 23 ,cursor:"pointer" }}
+            ></i>
+            <p>Your Cart is Empty</p>
+            <img alt="" src="./cart.gif" />
+          </div>
+        </Menu>
       </Navbar>
     </div>
   );

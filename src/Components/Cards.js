d93c78c9f162ca3e7debf5d4e5ea858f90f17187
Zeeshan1261 <zk12697@gmail.com>
2023-07-13 17:42:01 +0530

    updatingproject

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,12 +1,15 @@
-
 import React from "react";
 
 function Cards() {
-return(
-<div>
-cards
-</div>
-)
+  return (
+    <div>
+      cards
+      <h1>
+        Cards content currenly ,working on the project and updating the
+        repository daily
+      </h1>
+    </div>
+  );
 }
 
 export default Cards;
